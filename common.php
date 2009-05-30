fbe4050c9533d7a3a0ca7a8a8b46c2b1df0d5dc3
ivanlanin <ivanlanin@dc9f5906-571f-11de-8dc1-e798e446b60a>
2009-05-30 17:17:00 +0000

    statcounter and sitemeter

--- common.php
+++ common.php
@@ -105,12 +105,23 @@ function read_doc($file_name)
 
 function get_external_stat()
 {
+
+	// gostats
 	$ret .= '<!-- GoStats JavaScript Based Code -->';
 	$ret .= '<script type="text/javascript" src="http://gostats.com/js/counter.js"></script>';
 	$ret .= '<script type="text/javascript">_gos=\'gostats.com\';_goa=728945;_got=5;_goi=1;_goz=0;_gol=\'web traffic software\';_GoStatsRun();</script>';
 	$ret .= '<noscript><a target="_blank" title="web traffic software" href="http://gostats.com"><img alt="web traffic software" src="http://gostats.com/bin/count/a_728945/t_5/i_1/counter.png"  style="border-width:0" /></a></noscript>';
 	$ret .= '<!-- End GoStats JavaScript Based Code -->' . LF;
-	$ret .= '<script type="text/javascript"> var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www."); document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E")); </script> <script type="text/javascript"> try { var pageTracker = _gat._getTracker("UA-2254800-2"); pageTracker._trackPageview(); } catch(err) {}</script>';
+
+	// google analytics
+	$ret .= '<script type="text/javascript"> var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www."); document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E")); </script> <script type="text/javascript"> try { var pageTracker = _gat._getTracker("UA-2254800-2"); pageTracker._trackPageview(); } catch(err) {}</script>' . LF;
+
+	// statcounter
+	$ret .= '<!-- Start of StatCounter Code --> <script type="text/javascript"> var sc_project=4786656; var sc_invisible=1; var sc_partition=54; var sc_click_stat=1; var sc_security="f8096c17"; </script><script type="text/javascript" src="http://www.statcounter.com/counter/counter.js"></script><noscript><div class="statcounter"><a title="counter for blogspot" href="http://www.statcounter.com/blogger/" target="_blank"><img class="statcounter" src="http://c.statcounter.com/4786656/0/f8096c17/1/" alt="counter for blogspot" ></a></div></noscript> <!-- End of StatCounter Code -->' . LF;
+
+	// sitemeter
+	$ret .= '<!-- Site Meter --> <script type="text/javascript" src="http://s18.sitemeter.com/js/counter.js?site=s18kateglo"> </script> <noscript> <a href="http://s18.sitemeter.com/stats.asp?site=s18kateglo" target="_top"> <img src="http://s18.sitemeter.com/meter.asp?site=s18kateglo" alt="Site Meter" border="0"/></a> </noscript> <!-- Copyright (c)2009 Site Meter -->' . LF;
+
 	return($ret);
 }
 ?>
