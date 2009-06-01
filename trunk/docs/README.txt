3b239b26d3ff8af2d2c940dfaeb617ad447f9fe3
ivanlanin <ivanlanin@dc9f5906-571f-11de-8dc1-e798e446b60a>
2009-06-01 07:15:52 +0000

    Some fixes

--- trunk/docs/README.txt
+++ trunk/docs/README.txt
@@ -73,8 +73,7 @@ Kateglo.
 * Penyimpanan jika mengganti frasa gagal
 * Kesalahan KBBI: alur, apa
 * Algoritma parser KBBI belum efisien
-* Kesalahan parsing KBBI: "kokoh" -> redirection
-* Kesalahan parsing KBBI: "lepas" -> format salah
+* Kesalahan parsing KBBI: "kokoh", "lepas", "limut", "serok", "galah"
 
 * Kesalahan parsing KBBI: "uang", "tuju", "jabat", "areal"
   --> Diperbaiki pada versi 0.0.9
--- trunk/docs/data/0.0.13.sql
+++ trunk/docs/data/0.0.13.sql
@@ -1,5 +1,7 @@
 select a.phrase, count(*) from phrase a, definition b where a.phrase = b.phrase group by a.phrase order by 2 desc;
 
+select phrase, def_count from phrase where def_count = 0 order by phrase;
+
 alter table phrase add def_count int not null default 0 after ref_source;
 
 create index phrase on definition (phrase);
@@ -7,3 +9,13 @@ create index phrase on definition (phrase);
 update phrase set def_count = 0;
 update phrase a
 set a.def_count = (SELECT COUNT(b.def_uid) FROM definition b WHERE a.phrase = b.phrase);
+
+-- gelembung
+delete from phrase where phrase like '-gelembung%';
+delete from definition where phrase like '-gelembung%';
+delete from relation where root_phrase like '-gelembung%';
+delete from relation where related_phrase like '-gelembung%';
+delete from phrase where phrase like '-rektor%';
+delete from definition where phrase like '-rektor%';
+delete from relation where root_phrase like '-rektor%';
+delete from relation where related_phrase like '-rektor%';
