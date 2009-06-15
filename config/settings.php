509efb16391095c34d0be65d80c3d4fc8c12ff2f
ivanlanin <ivanlanin@dc9f5906-571f-11de-8dc1-e798e446b60a>
2009-06-15 04:04:17 +0000

    Version number: 0.0.20

--- config/settings.php
+++ config/settings.php
@@ -3,7 +3,7 @@
  * Default settings
  */
 // constants
-define(APP_VERSION, 'v0.0.19'); // application version. See README.txt
+define(APP_VERSION, 'v0.0.20'); // application version. See README.txt
 define(APP_NAME, 'Kateglo (Beta) - kamus, tesaurus, dan glosarium bahasa Indonesia'); // application name
 define(APP_SHORT, 'Kateglo (Beta)'); // application name
 define(LF, "\n"); // line break
