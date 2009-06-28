d65ae6000fad14dbf349ceda36c5316059c60ba6
ivanlanin <ivanlanin@dc9f5906-571f-11de-8dc1-e798e446b60a>
2009-06-28 05:38:59 +0000

    v0.01.20090628 * Umum: Alternatif logo dan tampilan. Nonaktif. * Kamus: Menambahkan KTG_TIMEOUT = 15 untuk membatasi waktu uji coba mengambil data dari KBBI

--- config/settings.php
+++ config/settings.php
@@ -3,10 +3,11 @@
  * Default settings
  */
 // constants
-define(APP_VERSION, 'v0.01.20090627'); // application version. See README.txt
+define(APP_VERSION, 'v0.01.20090628'); // application version. See README.txt
 define(APP_NAME, 'Kateglo (Beta) - kamus, tesaurus, dan glosarium bahasa Indonesia'); // application name
 define(APP_SHORT, 'Kateglo (Beta)'); // application name
 define(LF, "\n"); // line break
+define(KTG_TIMEOUT, 15); // timeout, used for curl
 
 // cleanup get
 foreach ($_GET as $key => $val) $_GET[$key] = trim($val);
