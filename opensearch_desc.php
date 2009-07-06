aa23ec8d7de456e0b202fa4b34b8a678ff179801
romihardiyanto <romihardiyanto@dc9f5906-571f-11de-8dc1-e798e446b60a>
2009-07-06 06:00:18 +0000

    Perbaikan ShortName dan Description agar lebih jelas dan tambahan elemen Url untuk autoupdate opensearch

--- opensearch_desc.php
+++ opensearch_desc.php
@@ -7,8 +7,9 @@ header('Content-type: text/xml');
 echo('<?xml version="1.0"?>' . "\n");
 ?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
-<ShortName>Kateglo</ShortName>
-<Description>Cari entri di Kateglo Bahtera</Description>
+<ShortName>Kateglo (Kamus)</ShortName>
+<Description>Cari entri kamus di Kateglo Bahtera</Description>
 <Image height="16" width="16" type="image/x-icon"><?php echo($base_url); ?>images/favicon.ico</Image>
 <Url type="text/html" method="get" template="<?php echo($base_url); ?>?mod=dictionary&amp;phrase={searchTerms}" />
+<Url type="application/opensearchdescription+xml" rel="self" template="<?php echo($base_url); ?>/opensearch_desc.php" />
 </OpenSearchDescription>
