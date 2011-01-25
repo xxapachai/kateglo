4bfbb41043f1166e6ea0d5faab04619fa86f746b
arthur.purnama <arthur.purnama@dc9f5906-571f-11de-8dc1-e798e446b60a>
2011-01-25 22:48:14 +0000

    Clean Up

--- branches/kateglox/application/utilities/SearchEngine.php
+++ branches/kateglox/application/utilities/SearchEngine.php
@@ -70,8 +70,8 @@ class SearchEngine implements interfaces\SearchEngine {
 		}
 		if ($this->service->ping ()) {
 			return $this->service;
-		}else {
-			throw new exceptions\SearchEngineException;
+		} else {
+			throw new exceptions\SearchEngineException ();
 		}
 	}
 	
@@ -86,6 +86,21 @@ class SearchEngine implements interfaces\SearchEngine {
 		} else {
 			$this->service = $service;
 		}
+	}
+	
+	/**
+	 * Simple Search interface
+	 *
+	 * @param string $query The raw query string
+	 * @param int $offset The starting offset for result documents
+	 * @param int $limit The maximum number of result documents to return
+	 * @param array $params key / value pairs for other query parameters (see Solr documentation), use arrays for parameter keys used more than once (e.g. facet.field)
+	 * @return Apache_Solr_Response
+	 *
+	 * @throws Exception If an error occurs during the service call
+	 */
+	public function search($query, $offset = 0, $limit = 10, $params = array(), $method = self::METHOD_GET) {
+		return $this->getSolrService ()->search ( $query, $offset, $limit, $params, $method );
 	}
 }
 
--- branches/kateglox/application/utilities/interfaces/SearchEngine.php
+++ branches/kateglox/application/utilities/interfaces/SearchEngine.php
@@ -47,6 +47,19 @@ interface SearchEngine {
 	 * @return void
 	 */
 	function setSolrService(\Apache_Solr_Service $service = null);
+	
+	/**
+	 * Simple Search interface
+	 *
+	 * @param string $query The raw query string
+	 * @param int $offset The starting offset for result documents
+	 * @param int $limit The maximum number of result documents to return
+	 * @param array $params key / value pairs for other query parameters (see Solr documentation), use arrays for parameter keys used more than once (e.g. facet.field)
+	 * @return Apache_Solr_Response
+	 *
+	 * @throws Exception If an error occurs during the service call
+	 */
+	function search($query, $offset = 0, $limit = 10, $params = array(), $method = self::METHOD_GET);
 
 }
 ?>
