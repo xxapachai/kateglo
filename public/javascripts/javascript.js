a4892e77fd48abe52dc93a7f02849c93cdf5cb84
arthur.purnama <arthur.purnama@dc9f5906-571f-11de-8dc1-e798e446b60a>
2011-03-15 16:23:58 +0000

    Ext4 ?

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,3 +1,9 @@
-/**
- * 
- */
+function import(source) {
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = source;
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+import('/ext4/ext-core-debug.js');
+import('/ext4/ext-all-debug.js');
