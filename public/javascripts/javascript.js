f2b4c7d0601c9e9ef94b350d4e1116383f8e6674
arthur.purnama <arthur.purnama@dc9f5906-571f-11de-8dc1-e798e446b60a>
2011-05-08 21:09:31 +0000

    Implementing Web Design for Start site Move Javascripts to the bottom

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,60 +1,28 @@
-Ext.BLANK_IMAGE_URL = '/images/s.gif';
-Ext.onReady(function() {
-    if (Ext.get('entry-random') != null) {
-        Ext.Ajax.defaultHeaders = {
-            'Accept': 'application/json'
-        };
-        Ext.Ajax.request({
+$(document).ready(function() {
+    if ($('#entry-random') != null){
+        $.ajax({
             url: '/',
-            success: function(response, opts) {
-                var obj = Ext.decode(response.responseText);
-                Ext.select('li.loader').remove();
-                //Ext.fly('entry-amount').insertHtml('afterBegin', obj.amount);
-                
-                Ext.each(obj.entry.docs, function(entry, index) {
-                    var definition = '';
-                    if(Ext.isArray(entry.definition)){
+            type: 'GET',
+            dataType: 'json',
+            success : function(data, status, xhr){
+                $('li.loader').remove();
+                $.each(data.entry.docs, function(index, entry){
+                    if($.isArray(entry.definition)){
                         definition = entry.definition[0];
                     }else{
                         definition = entry.definition;
                     }
-                    Ext.fly('entry-random').insertHtml('beforeEnd', '<li><strong><a href="entri/'+entry.entry+'">'+entry.entry+'</a></strong><p>'+definition+'</p></li>');
+                    $('#entry-random').append('<li><strong><a href="entri/'+entry.entry+'">'+entry.entry+'</a></strong><p>'+definition+'</p></li>');
+
                 });
 
-                Ext.each(obj.misspelled.docs, function(entry, index) {
-                    Ext.fly('misspelled-random').insertHtml('beforeEnd', '<li><strong><a href="entri/'+entry.spelled+'">'+entry.spelled+'</a></strong><p>bukan <a href="entri/'+entry.entry+'">'+entry.entry+'</a></p></li>');
+                $.each(data.misspelled.docs, function(index, entry) {
+                    $('#misspelled-random').append('<li><strong><a href="entri/' + entry.spelled + '">' + entry.spelled + '</a></strong><p>bukan <a href="entri/' + entry.entry + '">' + entry.entry + '</a></p></li>');
                 });
-
             },
-            failure: function(response, opts) {
-                console.log('server-side failure with status code ' + response.status);
+            error: function(xhr, status, errorThrown){
+                //console log? console.log('server-side failure with status code ' + response.status);
             }
-        });
-    }
-    if (Ext.select('h1.entry-header:first').elements.length != 0) {
-        var el = Ext.select('div.content');
-        var tabs = Ext.createWidget('tabpanel', {
-            renderTo: Ext.get('content-container'),
-            width: 600,
-            maxHeight: 5000,
-            minHeight: 600,
-            plain: true,
-            activeTab: 0,
-            border: 0,
-            bodyBorder: 0,
-            defaults :{
-                bodyPadding: 10
-            },
-            items: [
-                {
-                    title: 'Entri',
-                    contentEl: el.elements[0]
-                },
-                {
-                    title: 'Sunting',
-                    html: 'test'
-                }
-            ]
-        });
+        })
     }
 });
