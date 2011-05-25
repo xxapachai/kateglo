a239250fffa4713081ef3594f8af5abd0a626bc4
arthur.purnama <arthur.purnama@dc9f5906-571f-11de-8dc1-e798e446b60a>
2011-05-25 17:15:46 +0000

    Init CPanel

--- branches/kateglox/public/javascripts/cpanel/main.js
+++ branches/kateglox/public/javascripts/cpanel/main.js
@@ -4,21 +4,27 @@ Ext.onReady(function() {
     Ext.create('Ext.Viewport', {
         layout: {
             type: 'border',
-            padding: 5
+            padding: 0
         },
         defaults: {
-            split: true
+            split: false
         },
         items: [
             {
                 region: 'north',
                 height: 50,
+                border: false,
+                padding: '0 0 1 0',
                 html: 'north'
             },
             {
                 region: 'west',
+                split:true,
+                border: false,
+                animate: false,
+                activeOnTop: true,
                 collapsible: true,
-                title: 'West',
+                hideCollapseTool: true,
                 width: 250,
                 layout: 'accordion',
                 items:[
@@ -30,13 +36,32 @@ Ext.onReady(function() {
                         title: 'Panel 2',
                         id: 'panel2',
                         html: 'Content'
+                    },
+                    {
+                        title: 'Panel 3',
+                        id: 'panel3',
+                        html: 'Content'
                     }
                 ]
             },
             {
                 region: 'center',
+                xtype: 'tabpanel',
                 border: false,
-                html: 'test'
+                activeTab: 0,
+                defaults: {
+                    border: false
+                },
+                items: [
+                    {
+                        title: 'Tab 1',
+                        html : 'A simple tab'
+                    },
+                    {
+                        title: 'Tab 2',
+                        html : 'Another one'
+                    }
+                ]
             }
         ]
     });
