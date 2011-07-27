232dbea1c6f91b0ab2d0b3831609b8f10680974d
arthur.purnama <arthur.purnama@dc9f5906-571f-11de-8dc1-e798e446b60a>
2011-07-27 23:20:04 +0000

    Implementing Layout

--- branches/kateglox/application/services/Pagination.php
+++ branches/kateglox/application/services/Pagination.php
@@ -138,12 +138,12 @@ class Pagination implements interfaces\Pagination {
 			if ($pageMin > 1) { //create at least Prev Page element
 				if ($pageMin > 2) { //create First Page element
 					$page = 1;
-					$pageObject = new Page ( $page, $start, 'First', false );
+					$pageObject = new Page ( $page, $start, html_entity_decode('&laquo; Pertama', ENT_QUOTES, 'UTF-8'), false );
 					$pageCollection [] = $pageObject;
 				}
 				$page = $pageMin - 1;
 				$start = ($pageMin - 1) * $pagination->getLimit ();
-				$pageObject = new Page ( $page, $start, 'Prev', false );
+				$pageObject = new Page ( $page, $start, html_entity_decode('&lsaquo; Sebelumnya', ENT_QUOTES, 'UTF-8'), false );
 				$pageCollection [] = $pageObject;
 			}
 			for($i = ( int ) $pageMin; $i <= ( int ) $pageMax; $i ++) { //create numbered Page Elements
@@ -162,12 +162,12 @@ class Pagination implements interfaces\Pagination {
 			if ($pageMax < $pagination->getPageAmount ()) { //create Next Page Element
 				$page = $pageMax + 1;
 				$start = ($page - 1) * $pagination->getLimit ();
-				$pageObject = new Page ( $page, $start, 'Next', false );
+				$pageObject = new Page ( $page, $start, html_entity_decode('Berikutnya &rsaquo;', ENT_QUOTES, 'UTF-8'), false );
 				$pageCollection [] = $pageObject;
 				if ($page < $pagination->getPageAmount ()) { //create Last Page Element
 					$page = $pagination->getPageAmount ();
 					$start = ($page - 1) * $pagination->getLimit ();
-					$pageObject = new Page ( $page, $start, 'Last', false );
+					$pageObject = new Page ( $page, $start, html_entity_decode('Terakhir &raquo;', ENT_QUOTES, 'UTF-8'), false );
 					$pageCollection [] = $pageObject;
 				}
 			}
