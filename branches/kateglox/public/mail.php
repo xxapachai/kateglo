081f25ffb9c5344f6fc7ab3ceef291f889f6016f
arthur.purnama <arthur.purnama@dc9f5906-571f-11de-8dc1-e798e446b60a>
2012-01-19 20:04:48 +0000

    Refactoring zend framework

--- branches/kateglox/public/mail.php
+++ branches/kateglox/public/mail.php
@@ -3,8 +3,12 @@ $to = 'ubuntu@localhost';
 $subject = 'Test email using PHP';
 $message = 'This is a test email message';
 $headers = 'From: webmaster@localhost' . "\r\n" .
-   'Reply-To: webmaster@localhost' . "\r\n" .
-   'X-Mailer: PHP/' . phpversion();
+    'Reply-To: webmaster@localhost' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers, '-fwebmaster@localhost');
+if (mail($to, $subject, $message, $headers, '-fwebmaster@localhost')) {
+    echo "Mail sent successfully, check your mail!!!!";
+} else {
+    echo "Mail send failed! Check your configuration.";
+}
 ?> 
