5bde4afb5e5c81400d07c515cd7cb502eafebe26
arthur.purnama <arthur.purnama@dc9f5906-571f-11de-8dc1-e798e446b60a>
2012-07-04 15:55:39 +0000

    create puppet script for PHP mod and jetty

--- vagrant/puppet/modules/diverse/manifests/init.pp
+++ vagrant/puppet/modules/diverse/manifests/init.pp
@@ -1,7 +1,20 @@
 class diverse {
-  $packages = ["curl", "vim"]
-  package { $packages:
-    ensure => present,
-    require => Exec["apt-get update"]
-  }
+    $packages = ["curl", "vim", "subversion", "git", "libpcre3-dev", "make",
+                "augeas-tools", "libaugeas-dev", "libaugeas-ruby"]
+
+    package { $packages:
+        ensure => present,
+        require => Exec["apt-get update"]
+    }
+
+    # After installing augeas and augeas ruby, this step must be taken so
+    exec { "ln -s /usr/lib/ruby/1.8/x86_64-linux/_augeas.so /opt/vagrant_ruby/lib/ruby/site_ruby/1.8/x86_64-linux/":
+        creates => "/opt/vagrant_ruby/lib/ruby/site_ruby/1.8/x86_64-linux/_augeas.so",
+        require => [Package["augeas-tools"], Package["libaugeas-dev"], Package["libaugeas-ruby"]],
+    }
+
+    exec { "ln -s /usr/lib/ruby/1.8/augeas.rb /opt/vagrant_ruby/lib/ruby/site_ruby/1.8/":
+        creates => "/opt/vagrant_ruby/lib/ruby/site_ruby/1.8/augeas.rb",
+        require => [Package["augeas-tools"], Package["libaugeas-dev"], Package["libaugeas-ruby"]],
+    }
 }
